d00f3e4e7f2e36395469dbcaf2ecd821f84b3faa
Anton Dryakhlykh <flowt@hotmail.com>
2017-03-31 16:28:24 +0300

    initial commit

---
+++ utair/Classes/Application/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  utair
+//
+//  Created by Anton Dryakhlykh on 31.03.17.
+//  Copyright © 2017 teedee. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ utair/Classes/Application/Blocks.swift
@@ -0,0 +1,9 @@
+//
+//  Blocks.swift
+//  utair
+//
+//  Created by Anton Dryakhlykh on 31.03.17.
+//  Copyright © 2017 teedee. All rights reserved.
+//
+
+import Foundation
